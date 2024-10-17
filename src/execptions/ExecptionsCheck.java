4fe82ba78fba9e6e6934398fae88a60b446d9cdb
utkoj <justyna.utko@ttpsc.com>
2024-10-17 17:29:45 +0200

    podrasowane wyjątiki :D

--- src/execptions/ExecptionsCheck.java
+++ src/execptions/ExecptionsCheck.java
@@ -5,24 +5,32 @@ import java.util.Scanner;
 public class ExecptionsCheck {
 
 
-    public static void main(String[] args) throws InvalidAgeExecption{
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj wiek ");
-        int wiek = scanner.nextInt();
+    public static void main(String[] args) {
 
 
-        if (wiek <0) {
-            throw new InvalidAgeExecption("wiek jest niepoprawny < 0 ");
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Podaj wiek ");
+
+            int wiek = scanner.nextInt();
+
+            if (wiek < 0) {
+                System.out.println("wiek jest niepoprawny < 0 ");
+            }
+            if (wiek >= 18) {
+                System.out.println("Jesteś pełnoletni");
+            } else {
+                System.out.println("Nie jesteś pełnoletni");
+            }
+            System.out.println("Twój wiek " + wiek);
+
+        } catch (Exception e) {
+            //throw new RuntimeException(e);
+            System.out.println("wyjatek " + e.getMessage());
+            System.out.println(e.fillInStackTrace());
+        }finally {
+            System.out.println("DUPA");
         }
-
-        if (wiek>= 18) {
-            System.out.println("Jesteś pełnoletni");
-        } else {
-            System.out.println("Nie jesteś pełnoletni");
-        }
-
-        System.out.println("Twój wiek " + wiek );
     }
 
 
